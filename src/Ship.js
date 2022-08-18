d83f3b80dd7db585728d0a5e5449e290c3234701
fufako <patryko.mikiciuk@gmail.com>
2022-08-18 11:53:27 +0200

    Create Ship class and add tests

---
+++ src/Ship.js
@@ -0,0 +1,15 @@
+export class Ship {
+  constructor(length) {
+    this.length = length
+    this.hits = []
+  }
+  hit(position) {
+    if (this.hits.includes(position) || position < 0 || position >= this.length)
+      return
+
+    this.hits.push(position)
+  }
+  isSunk() {
+    return this.hits.length === this.length
+  }
+}
---
+++ src/tests/ship.test.js
@@ -0,0 +1,26 @@
+import { Ship } from "../Ship"
+
+//Lepsza metoda niż tworzenie ship w każdym tescie?
+
+test("creates and initializes a ship", () => {
+  let ship = new Ship(3)
+  expect(ship).toEqual({ length: 3, hits: [] })
+})
+test("ship takes a hit", () => {
+  let ship = new Ship(3)
+  ship.hit(2)
+  expect(ship.hits).toContain(2)
+})
+test("ship sinks", () => {
+  let ship = new Ship(3)
+  ship.hit(0)
+  ship.hit(1)
+  ship.hit(2)
+  expect(ship.isSunk()).toBe(true)
+})
+test("prevents being hit multiple times at the same spot", () => {
+  let ship = new Ship(3)
+  ship.hit(1)
+  ship.hit(1)
+  expect(ship.hits.length).toBe(1)
+})

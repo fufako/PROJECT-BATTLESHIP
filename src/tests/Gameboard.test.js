041fc9efbc6a20edc9a09cdee9e679f36538d0c3
fufako <patryko.mikiciuk@gmail.com>
2022-08-26 16:25:33 +0200

    Remove not usefull tests

--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -25,21 +25,3 @@ test("Check if user can place a ship", () => {
   let gameBoard = new GameBoard()
   expect(gameBoard.checkShipPlacement(5, 1, 1)).toEqual(true)
 })
-
-test("Check if ship is placing", () => {
-  let gameBoard = new GameBoard()
-  let ship = new Ship(5)
-  expect(gameBoard.placeShip(ship, 0, 0)).toEqual("Battleship")
-})
-test("Add to missed attacks", () => {
-  let gameBoard = new GameBoard()
-  let ship = new Ship(5)
-  gameBoard.receiveAttack(2, 5)
-  expect(gameBoard.getMissedAttacks()).toEqual([{ x: 2, y: 5 }])
-})
-test("Successful attack", () => {
-  let gameBoard = new GameBoard()
-  let ship = new Ship(5)
-  gameBoard.placeShip(ship, 0, 0)
-  expect(gameBoard.receiveAttack(0, 0)).toEqual("Battleship")
-})
